ad8984b8d12fdea8b3ac6e67ebb4ebfab14a2c87
axelcall123 <axel15calderon@gmail.com>
2020-08-21 16:22:12 -0600

    Absolute path

--- TAREA2/main.py
+++ TAREA2/main.py
@@ -3,7 +3,14 @@ import os
 import json
 import xml.etree.ElementTree as ET
 
-with open(os.path.abspath("TAREA2/names.csv")) as file:
+import os.path
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+path_1 = os.path.join(my_path, "../TAREA2/names.csv")
+path_2 = os.path.join(my_path, "../TAREA2\dat.json")
+path_3 = os.path.join(my_path, "../TAREA2\dat.xml")
+
+with open(path_1) as file:
     csvss = csv.reader(file)
     #print(file)
     for reg in csvss:
@@ -12,10 +19,10 @@ print("tipo: ", type(csvss))
 
 print("------------------------------")
 def readJson():
-    file=open(os.path.abspath("TAREA2/dat.json"))
+    file=open(path_2)
     data=json.load(file)
     file.close()
-    print(data)
+    #print(data)
     print(type(data))
     return(data)
 
@@ -24,7 +31,7 @@ for i in dict:
     print(i)
 
 print("------------------------------")
-xml= ET.parse(os.path.abspath("TAREA2/dat.xml"))
+xml= ET.parse(path_3)
 archivo=xml.getroot()
 
 for a in archivo:
