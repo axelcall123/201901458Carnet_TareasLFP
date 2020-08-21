d2a70e7e2fd56d5be336dd61e7b28108dd5d579c
axelcall123 <axel15calderon@gmail.com>
2020-08-21 16:24:48 -0600

    ---

--- TAREA2/main.py
+++ TAREA2/main.py
@@ -7,8 +7,8 @@ import os.path
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 path_1 = os.path.join(my_path, "../TAREA2/names.csv")
-path_2 = os.path.join(my_path, "../TAREA2\dat.json")
-path_3 = os.path.join(my_path, "../TAREA2\dat.xml")
+path_2 = os.path.join(my_path, "../TAREA2/dat.json")
+path_3 = os.path.join(my_path, "../TAREA2/dat.xml")
 
 with open(path_1) as file:
     csvss = csv.reader(file)
