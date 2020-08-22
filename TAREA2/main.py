6c46b6beefc5cc6915657729cd2c3fd0f77c22b8
axelcall123 <axel15calderon@gmail.com>
2020-08-21 23:09:23 -0600

    END

--- TAREA2/main.py
+++ TAREA2/main.py
@@ -12,7 +12,7 @@ path_3 = os.path.join(my_path, "../TAREA2/dat.xml")
 
 with open(path_1) as file:
     csvss = csv.reader(file)
-    #print(file)
+    print(file)
     for reg in csvss:
         print(reg[0], reg[1], reg[2], reg[3])
 print("tipo: ", type(csvss))
@@ -22,7 +22,7 @@ def readJson():
     file=open(path_2)
     data=json.load(file)
     file.close()
-    #print(data)
+    print(data)
     print(type(data))
     return(data)
 
